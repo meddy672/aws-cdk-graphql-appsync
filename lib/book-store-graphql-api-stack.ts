b6d0bc5736f4a194b35f968ac7cbaa279c0eb6f9
Matthew Eddy <meddy672@gmail.com>
2021-12-13 08:44:29 -0500

    added dynamodb construct and set permissions

--- lib/book-store-graphql-api-stack.ts
+++ lib/book-store-graphql-api-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export class BookStoreGraphqlApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -22,17 +23,31 @@ export class BookStoreGraphqlApiStack extends cdk.Stack {
       xrayEnabled: true
     });
 
+    const booksTable = new dynamodb.Table(this, 'BooksTable', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    })
+
     const listBooksHandler = new lambda.Function(this, 'BookStoreGraphqlApiHandler', {
       code: lambda.Code.fromAsset('functions'),
       runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'listBooks.handler'
+      handler: 'listBooks.handler',
+      environment: {
+        BOOKS_TABLE: booksTable.tableName,
+      }
     });
+
+    booksTable.grantReadData(listBooksHandler);
 
     const listBooksDataSource = api.addLambdaDataSource("listBooksDataSource", listBooksHandler);
 
     listBooksDataSource.createResolver({
       typeName: "Query",
       fieldName: "listBooks"
-    })
+    });
+
   }
 }
