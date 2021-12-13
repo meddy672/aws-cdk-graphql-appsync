28f491afbd7d2c39df0a78cf6730eabf5c54f52d
Matthew Eddy <meddy672@gmail.com>
2021-12-13 09:44:15 -0500

    setup lambda to read items from dynamodb

--- functions/listBooks.ts
+++ functions/listBooks.ts
@@ -1,18 +1,29 @@
-export const handler = async () => {
-    return [
-        {
-            id: "abc-123",
-            title: "The Hobbit",
-            completed: true,
-            rating: 10,
-            reviews: ["The best book ever wriitend"]
-        },
-        {
-            id: "def-456",
-            title: "A terrible book",
-            completed: true,
-            rating: 2,
-            reviews: ["What did I just read"]
+import { AppSyncResolverHandler } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+
+type Book = {
+    id: string;
+    title: string;
+    completed?: string;
+    rating?: string;
+    reviews?: string[];
+};
+
+const docClient = new DynamoDB.DocumentClient()
+export const handler: AppSyncResolverHandler<null, Book[] | null> = async () => {
+    try {
+        if (!process.env.BOOKS_TABLE) {
+            console.error("Books table was not defined");
+            return null;
         }
-    ]
+
+        const data = await docClient.scan({
+            TableName: process.env.BOOKS_TABLE
+        }).promise();
+
+        return data.Items as Book[];
+    } catch (err: any) {
+        console.error("Database error occured", err.message);
+        return null;
+     }
 }
